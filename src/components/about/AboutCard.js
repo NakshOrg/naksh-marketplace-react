e5ec7294ef8a76dded82ceb9ab446784177e739a
Hemanthkumaran <hemanthkumaran007@gmail.com>
2022-01-24 23:30:38 +0530

    about page ui fixes

--- src/components/about/AboutCard.js
+++ src/components/about/AboutCard.js
@@ -2,17 +2,17 @@ import React from 'react';
 
 import classes from './about.module.css';
 
-export default function AboutCard() {
+export default function AboutCard({ title, content }) {
     return (
         // <div className={classes.weOfferCardContainer}>
             <div className={classes.weOfferCardWrapper}>
             {/* <div className={classes.weOfferCardWrapperBlack}></div> */}
-                <div className={classes.weOfferCardItem}>
+                {/* <div className={classes.weOfferCardItem}> */}
                     <div className={classes.weOfferCardHoverItemContent}>
-                        <div className={classes.weOfferCardTitle}>Support</div>
-                        <div className={classes.weOfferCardDescription}>Purchase artworks created by vernacular and digital artists across India</div>
+                        <div className={classes.weOfferCardTitle}>{title}</div>
+                        <div className={classes.weOfferCardDescription}>{content}</div>
                     </div>
-                </div>
+                {/* </div> */}
             </div>
         // </div>
     )
